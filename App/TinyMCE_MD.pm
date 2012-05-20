c27380a57ede32e7e533707ceb1dd2144e4733d1
John D Jones III <jnbek1972@gmail.com>
2012-05-19 20:24:16 -0600

    Initial Commit: TinyMCE -> Markdown

---
+++ App/TinyMCE_MD.pm
@@ -0,0 +1,18 @@
+package TinyMCE_MD
+
+use strict;
+use warnings;
+
+use base qw(CGI:Ex::App);
+use CGI::Ex::Dump qw(debug);
+use HTML::WikiConverter;
+
+use FindBin;
+our $base_path = $FindBin::RealBin;
+
+sub main_hash_swap {
+    my $self = shift;
+}
+sub main_finalize {
+    my $self = shift;
+}
